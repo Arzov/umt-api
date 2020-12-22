fc6d71dc3d4d8a950ae633d8a73e1585ae46f23a
Franco Barrientos <fjbarrientosg@gmail.com>
2020-12-22 19:58:49 -0300

    Campos pueden ser nulos en entidades graphQL

--- backend/aws/lambda/functions/umt-get-user/utils/dql.js
+++ backend/aws/lambda/functions/umt-get-user/utils/dql.js
@@ -22,20 +22,24 @@ const getUser = (db, tableName, hashKey, rangeKey, fn) => {
     }, function(err, data) {
         if (err) return fn(err);
         else
-            fn(null, {
-                email: data.Item.hashKey.S.split('#')[1],
-                geohash: data.Item.geohash.S,
-                coords: data.Item.coords.M,
-                genderFilter: data.Item.genderFilter.SS,
-                ageMinFilter: data.Item.ageMinFilter.N,
-                ageMaxFilter: data.Item.ageMaxFilter.N,
-                matchFilter: data.Item.matchFilter.SS,
-                positions: data.Item.positions.SS,
-                skills: data.Item.skills.S,
-                foot: data.Item.foot.S,
-                weight: data.Item.weight.N,
-                height: data.Item.height.N
-            });
+            if (Object.keys(data).length === 0 && data.constructor === Object) {
+                fn(null, {});
+            } else {
+                fn(null, {
+                    email: data.Item.hashKey.S.split('#')[1],
+                    geohash: data.Item.geohash.S,
+                    coords: data.Item.coords.M,
+                    genderFilter: data.Item.genderFilter.SS,
+                    ageMinFilter: data.Item.ageMinFilter.N,
+                    ageMaxFilter: data.Item.ageMaxFilter.N,
+                    matchFilter: data.Item.matchFilter.SS,
+                    positions: data.Item.positions.SS,
+                    skills: data.Item.skills.S,
+                    foot: data.Item.foot.S,
+                    weight: data.Item.weight.N,
+                    height: data.Item.height.N
+                });
+            }
     });
 }
 
